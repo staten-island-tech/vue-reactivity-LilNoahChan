5f5fb9b57e543210157971c17431834fabcb9f0d
LilNoahChan <noahchan305@gmail.com>
2023-03-10 13:12:59 -0500

    e

--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data(){
+    return{
+      student: "Harry",
+      graduated: false,
+      animals: ["Pig", "Horse", "Donkey", "Cow", "Duck"]
+    }
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<div class="home">
+  <h1 v-if="graduated">{{student}}</h1>
+  <h1 v-else>"They didnt gradate"</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
+
+</div>
 </template>
+
+<style>
+h1{
+  color: red;
+}
+</style>
