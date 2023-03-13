1bd2e8360224d3a4b38e995ef3c8e34b4ac8f511
LilNoahChan <noahchan305@gmail.com>
2023-03-13 13:15:01 -0400

    313

--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -4,23 +4,43 @@ export default {
   components: {},
   data(){
     return{
-      student: "Harry",
-      graduated: false,
-      animals: ["Pig", "Horse", "Donkey", "Cow", "Duck"]
-    }
-  }
-}
+      message: "",
+      // loggedIn: false,
+      // student: "Harry",
+      // graduated: true,
+      // animals: ["Pig", "Horse", "Donkey", "Cow", "Duck"]
+    };
+  },
+  methods: {},
+  // methods: {
+  //   authState: function () {
+  //     if(this.loggedIn===false){
+  //       this.loggedIn = true;
+  //     } else {
+  //       this.loggedIn = false;
+  //     }
+  //   },
+  // },
+};
 </script>
 
 <template>
-<div class="home">
+  <div class="home">
+    <input type="text" placeholder="edit me" v-model="message" />
+    <p>{{ message }}</p>
+  </div>
+  <!-- <div class="home">
+    <button v-on:click="authState" v-if="loggedIn">Log Out</button>
+    <button @click="authState" v-else>Login</button>
+  </div> -->
+<!-- <div class="home">
   <h1 v-if="graduated">{{student}}</h1>
   <h1 v-else>"They didnt gradate"</h1>
   <ul>
     <li v-for="animal in animals" :key="animal">{{ animal }}</li>
   </ul>
 
-</div>
+</div> -->
 </template>
 
 <style>
