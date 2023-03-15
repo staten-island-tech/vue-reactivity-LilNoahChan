ba84e9db2378e011665e8031de3356ae216b811d
LilNoahChan <noahchan305@gmail.com>
2023-03-15 13:18:17 -0400

    a

--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -4,12 +4,18 @@ export default {
   components: {},
   data(){
     return{
-      message: "",
+      selected: "",
       // loggedIn: false,
       // student: "Harry",
       // graduated: true,
       // animals: ["Pig", "Horse", "Donkey", "Cow", "Duck"]
     };
+  },
+  computed:{
+    now: function(){
+      const time = new Date();
+      return `${time.getHours}:${time.getMinutes}`;
+    },
   },
   methods: {},
   // methods: {
@@ -24,15 +30,35 @@ export default {
 };
 </script>
 
+<style>
+button {
+  font-size: 30px;
+}
+</style>
+
 <template>
   <div class="home">
+    <h2>{{ now }}</h2>
+  </div>
+  <!-- <div class="home">
+    <select v-model="selected">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>{{ selected }}</span>
+  </div> -->
+
+  <!-- <div class="home">
     <input type="text" placeholder="edit me" v-model="message" />
     <p>{{ message }}</p>
-  </div>
+  </div> -->
+
   <!-- <div class="home">
     <button v-on:click="authState" v-if="loggedIn">Log Out</button>
     <button @click="authState" v-else>Login</button>
   </div> -->
+
 <!-- <div class="home">
   <h1 v-if="graduated">{{student}}</h1>
   <h1 v-else>"They didnt gradate"</h1>
