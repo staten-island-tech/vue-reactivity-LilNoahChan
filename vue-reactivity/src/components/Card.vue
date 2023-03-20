89beb473c293cc3aa6fc28beb728bbf9e06382db
LilNoahChan <noahchan305@gmail.com>
2023-03-20 13:13:33 -0400

    arra

---
+++ vue-reactivity/src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+<h2>Ttile</h2>
+<img src="" alt="">
+<p></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    items:{
+      title: "Soap",
+      description: "Liquid",
+      price: 2,
+    },
+    {
+      title: "Soap",
+      description: "Liquid",
+      price: 2,
+    }
+  }
+</script>
+
+<style>
+
+</style>
