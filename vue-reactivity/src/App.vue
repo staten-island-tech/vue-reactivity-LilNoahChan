e9ce7f86f4d8103591a6e6110d0adc8b38e9e072
LilNoahChan <noahchan305@gmail.com>
2023-03-30 22:57:07 -0400

    new start

---
+++ vue-reactivity/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/icons/HelloWorld.vue'
+</script>
+
+<template>
+<h1>CVS SHOPPING LIST</h1>
+
+  <RouterView />
+</template>
+
+<style scoped>
+h1 {
+  font-size: 5vh;
+  margin: auto;
+  text-align: center;
+  margin-top: 2px;
+  font-weight: bold;
+  color: white;
+}
+</style>
---
+++ vue-reactivity/src/components/icons/Card.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+  name: 'Card',
+  props: {
+    section: String,
+    itemName: String,
+    itemCost: Number,
+    image: String,
+  }
+
+}
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
